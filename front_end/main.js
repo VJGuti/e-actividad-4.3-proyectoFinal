0486dbcb0e43ec82b321d4d1db64caff1ec9ddd0
mendezleoa <92312161+mendezleoa@users.noreply.github.com>
2023-08-05 15:44:35 -0400

    Agregado modal de actualización de información

--- front_end/main.js
+++ front_end/main.js
@@ -1,15 +1,27 @@
-const modal = document.querySelector("#modal");
-const btnAbrirModal = document.querySelector("#btn-abrir-modal");
-const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
-const formAgregar = document.querySelector("#form-agregar")
+const modal1 = document.querySelector("#modal1");
+const btnAbrirModal1 = document.querySelector("#btn-abrir-modal1");
+const btnCerrarModal1 = document.querySelector("#btn-cerrar-modal1");
+
+const modal2 = document.querySelector("#modal2");
+const btnAbrirModal2 = document.querySelector("#btn-abrir-modal2");
+const btnCerrarModal2 = document.querySelector("#btn-cerrar-modal2");
+
+const formAgregar = document.querySelector("#form-agregar");
+const formActualizar = document.querySelector("#form-actualizar");
 
 /* Abrir y cerrar el modal de formulario */
-btnAbrirModal.addEventListener("click", () => {
-  modal.showModal();
+btnAbrirModal1.addEventListener("click", () => {
+  modal1.showModal();
+});
+btnAbrirModal2.addEventListener("click", () => {
+  modal2.showModal();
 });
 
-btnCerrarModal.addEventListener("click", () => {
-  modal.close();
+btnCerrarModal1.addEventListener("click", () => {
+  modal1.close();
+});
+btnCerrarModal2.addEventListener("click", () => {
+  modal2.close();
 });
 
 const $foto = document.querySelector("#foto"),
@@ -25,6 +37,8 @@ $foto.addEventListener("change", () => {
   $fotoPrevisualizacion.src = objectURL;
 });
 
+const $foto2 = document.querySelector("#foto2");
+
 /* Guardar el formulario */
 formAgregar.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -38,4 +52,18 @@ formAgregar.addEventListener("submit", (e) => {
   const fotoCargada = fotoPrevisualizacion.src
   datos = {nombre, categoria, precio, cantidad, descripcion, clasificacion, fotoCargada};
   console.log(datos);
-})
+});
+
+formActualizar.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let datos;
+  const nombre = document.querySelector("#nombre-act").value;
+  const categoria = document.querySelector("#categoria-act").value;
+  const precio = document.querySelector("#precio-act").value;
+  const cantidad = document.querySelector("#cantidad-act").value;
+  const descripcion = document.querySelector("#descripcion-act").value;
+  const clasificacion = document.querySelector("#clasificacion-act").value;
+  const fotoCargada = fotoPrevisualizacionAct.src
+  datos = {nombre, categoria, precio, cantidad, descripcion, clasificacion, fotoCargada};
+  console.log(datos);
+});
